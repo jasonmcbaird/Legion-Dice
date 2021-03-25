b95060a8180f08417adcbce15ecbce77a714024f
Jason Baird <jason_baird@trimble.com>
2021-03-25 14:58:52 -0600

    Add progress bar

--- Shared/SimulateRow.swift
+++ Shared/SimulateRow.swift
@@ -8,8 +8,15 @@
 import SwiftUI
 
 struct SimulateRow: View {
+  private let expectedSimulationCount = 5000
+  
   @EnvironmentObject var configuration: Configuration
   @State var simulations: [Simulation] = []
+  var completedSimulations: Float {
+    Float(simulations.count) / Float(expectedSimulationCount)
+  }
+  @State var simulationInProgress = true
+  
   var averageDamage: Float {
     guard !simulations.isEmpty else { return 0 }
     return Float(simulations.map { $0.wounds }.reduce(0, +)) / Float(simulations.count)
@@ -17,13 +24,27 @@ struct SimulateRow: View {
   
   var body: some View {
     HStack {
-      Button("Simulate") {
-        simulations = Array(repeating: (), count: 10000).map { Simulation(configuration: configuration) }
-      }.foregroundColor(.white)
-      .padding()
-      .background(Color.blue)
-      .cornerRadius(15)
-      Text("Average damage: \(averageDamage, specifier: "%.3f")")
+      VStack {
+        Button("Simulate") {
+          simulations = []
+          simulationInProgress = true
+          DispatchQueue(label: "Simulate").async {
+            for _ in 0..<expectedSimulationCount {
+              simulations.append(Simulation(configuration: configuration))
+            }
+            simulationInProgress = false
+          }
+        }.foregroundColor(.white)
+        .padding()
+        .background(Color.blue)
+        .cornerRadius(15)
+        ProgressView(value: completedSimulations)
+          .progressViewStyle(LinearProgressViewStyle())
+          .cornerRadius(5)
+          .opacity(simulationInProgress ? 1 : 0)
+      }
+      Spacer()
+      Text("Average Wounds: \(averageDamage, specifier: "%.3f")")
     }.padding()
   }
 }
